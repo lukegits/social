bf8894a8f13b39efa969b91e28a688adc54fd5fd
Lukegits <luket.chavez@gmail.com>
2018-11-15 13:57:13 -0700

    added user sign in and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :discussions
+  root 'discussions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
